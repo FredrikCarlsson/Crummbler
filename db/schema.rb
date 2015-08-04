b143baff0159f8b02b369e467353aacea0eee531
Fredrik Carlsson <Fredrikvarnamo@hotmail.com>
2015-08-04 12:57:25 +0200

    Added so that image can be uploaded

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803152405) do
+ActiveRecord::Schema.define(version: 20150804094805) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "kind"
   end
 
 end
